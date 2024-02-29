f5a232bed85b5033fe1430f10cdffdddaa18717b
s.shahmohammadi <s.shahmohammadi@myarman.com>
2024-02-29 12:55:21 +0330

    added linters

--- mobiles/apps.py
+++ mobiles/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class MobilesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'mobiles'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "mobiles"
--- mobiles/models.py
+++ mobiles/models.py
@@ -1,4 +1,5 @@
 from django.db import models
 
+
 class Mobile(models.Model):
     name = models.BigAutoField()
